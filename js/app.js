ebd750b1a1dca73e1b4386df141bbe470ea00e7c
eliassbaranovs <82933682+eliassbaranovs@users.noreply.github.com>
2025-06-26 17:18:05 +0300

    Fix navigation closing toggle on large screens

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ allLinks.forEach(function (link) {
 
     // Close mobile naviagtion
     if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
+      headerEl.classList.remove("nav-open");
   });
 });
 
